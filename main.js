76088e71b67c9e76c7bf9a1a182b3ab161118e3c
Eze Gatica <ezequielgatica@gmail.com>
2021-09-24 21:33:07 -0300

    redirect

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 const { Player } = require('discord-player');
 const { Client, Intents } = require('discord.js');
-
+const express = require('express');
+const app = express();
+app.listen(80);
+app.all('*', function (req, res) {
+    res.redirect("https://discord.com/api/oauth2/authorize?client_id=702882808422727761&permissions=3206224&scope=bot")
+});
 global.client = new Client({
     intents: [
         Intents.FLAGS.GUILDS,
